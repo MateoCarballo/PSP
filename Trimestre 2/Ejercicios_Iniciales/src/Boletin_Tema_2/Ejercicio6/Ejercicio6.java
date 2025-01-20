32466aa331cbe8adc84e5ff0a0c1ffeb245558b2
Mateo Carballo Alonso <a22mateoca@teis.intranet>
2025-01-20 20:49:56 +0100

    Completada tarea 6 boletin 2

---
+++ Trimestre 2/Ejercicios_Iniciales/src/Boletin_Tema_2/Ejercicio6/Ejercicio6.java
@@ -0,0 +1,13 @@
+package Boletin_Tema_2.Ejercicio6;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+       /*
+        HiloFactorial hfactorial = new HiloFactorial();
+        hfactorial.start();
+
+        */
+        HiloFibonacci hiloFibonacci = new HiloFibonacci();
+        hiloFibonacci.run();
+    }
+}
